3ad7028920b977767aa961a0edb340ff4ce4504f
samer <samaladhami@gmail.com>
2017-01-04 22:58:56 -0600

    template

--- can.js
+++ can.js
@@ -1,9 +1,21 @@
 var Controller = can.Control({
    init: function(element , options){
 
-     element.html('It works!!')
+     element.empty()
+     element.html( can.view('myControlTemplate') )
 
    }
 })
 
+var Controller2 = can.Control({
+
+  init: function(elm , options){
+
+    elm.html('from Contrller2')
+
+  }
+
+})
+
 var myControl = new Controller('#container')
+var myControl2 = new Controller2('#container2')
